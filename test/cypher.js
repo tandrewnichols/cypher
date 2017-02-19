c57a539ed0543c0bde7f63faebef61c2e1d58d81
Andrew Nichols <tandrewnichols@gmail.com>
2017-02-19 12:07:00 -0500

    Add match

--- test/cypher.js
+++ test/cypher.js
@@ -1,3 +1,40 @@
 describe('cypher', function() {
-  var subject = require('../lib/cypher');
+  let Cypher = require('../lib/cypher');
+  
+  describe('.match', function() {
+    it('should match a simple node', function() {
+      let str = new Cypher().match('n').toString();
+      str.should.equal('MATCH (n)')
+    });
+
+    it('should match a node with a label', function() {
+      let str = new Cypher().match('n', 'Person').toString();
+      str.should.equal('MATCH (n:Person)');
+    });
+
+    it('should match a node with a label and properties', function() {
+      let str = new Cypher().match('n', 'Person', { name: 'John' }).toString();
+      str.should.equal("MATCH (n:Person { name: 'John' })");
+    });
+
+    it('should match a node with properties and no label', function() {
+      let str = new Cypher().match('n', { name: 'John' }).toString();
+      str.should.equal("MATCH (n { name: 'John' })");
+    });
+
+    it('should match a node with a label where the node is unnamed', function() {
+      let str = new Cypher().match('Person').toString();
+      str.should.equal('MATCH (:Person)');
+    });
+
+    it('should match a node with properties where the node is unnamed', function() {
+      let str = new Cypher().match({ name: 'John' }).toString();
+      str.should.equal("MATCH ({ name: 'John' })");
+    });
+
+    it('should match a node with a label and properties where the node is unnamed', function() {
+      let str = new Cypher().match('Person', { name: 'John' }).toString();
+      str.should.equal("MATCH (:Person { name: 'John' })");
+    });
+  });
 });
