d73312e5d314f71c5a3ad75fa7ac325a5e56a148
Andrew Nichols <tandrewnichols@gmail.com>
2017-02-19 22:35:44 -0500

    More work on matches

--- test/cypher.js
+++ test/cypher.js
@@ -3,38 +3,108 @@ describe('cypher', function() {
   
   describe('.match', function() {
     it('should match a simple node', function() {
-      let str = new Cypher().match('n').toString();
+      let str = new Cypher().match('n').build();
       str.should.equal('MATCH (n)')
     });
 
-    it('should match a node with a label', function() {
-      let str = new Cypher().match('n', 'Person').toString();
-      str.should.equal('MATCH (n:Person)');
-    });
-
-    it('should match a node with a label and properties', function() {
-      let str = new Cypher().match('n', 'Person', { name: 'John' }).toString();
-      str.should.equal("MATCH (n:Person { name: 'John' })");
-    });
-
-    it('should match a node with properties and no label', function() {
-      let str = new Cypher().match('n', { name: 'John' }).toString();
+    it('should match a node with properties', function() {
+      let str = new Cypher().match('n', { name: 'John' }).build();
       str.should.equal("MATCH (n { name: 'John' })");
     });
 
-    it('should match a node with a label where the node is unnamed', function() {
-      let str = new Cypher().match('Person').toString();
-      str.should.equal('MATCH (:Person)');
-    });
-
     it('should match a node with properties where the node is unnamed', function() {
-      let str = new Cypher().match({ name: 'John' }).toString();
+      let str = new Cypher().match({ name: 'John' }).build();
       str.should.equal("MATCH ({ name: 'John' })");
     });
+  });
 
-    it('should match a node with a label and properties where the node is unnamed', function() {
-      let str = new Cypher().match('Person', { name: 'John' }).toString();
-      str.should.equal("MATCH (:Person { name: 'John' })");
+  describe('.withLabel', function() {
+    it('should set the type of a previous node', function() {
+      let str = new Cypher().match('n').withLabel('Person').build();
+      str.should.equal('MATCH (n:Person)');
+    });
+
+    it('should allow an array of labels', function() {
+      let str = new Cypher().match('n').withLabel(['Person', 'Mailman']).build();
+      str.should.equal('MATCH (n:Person:Mailman)');
+    });
+
+    it('should set the type of a previous node connected by relationship', function() {
+      let str = new Cypher().match('n').withRelationship('r').withLabel('Person').build();
+      str.should.equal('MATCH (n)-[r]-(:Person)');
+    });
+  });
+
+  describe('.withType', function() {
+    it('should set the type of a previous relationship', function() {
+      let str = new Cypher().match('n').withRelationship('r').withType('Person').build();
+      str.should.equal('MATCH (n)-[r:Person]-()');
+    });
+
+    it('should allow an array of types', function() {
+      let str = new Cypher().match('n').withRelationship('r').withType(['Person', 'Mailman']).build();
+      str.should.equal('MATCH (n)-[r:Person|:Mailman]-()');
+    });
+
+    it('should wrap types with spaces in backticks', function() {
+      let str = new Cypher().match('n').withRelationship('r').withType('Software Engineer').build();
+      str.should.equal(`MATCH (n)-[r:\`Software Engineer\`]-()`);
+    });
+  });
+
+  describe('.withRelationship', function() {
+    it('should relate to nodes without direction', function() {
+      let str = new Cypher().match('n').withRelationship().build();
+      str.should.equal('MATCH (n)--()');
+    });
+
+    it('should accept a named relationship', function() {
+      let str = new Cypher().match('n').withRelationship('r').build();
+      str.should.equal('MATCH (n)-[r]-()');
+    });
+
+    it('should accept a named relationship and properties', function() {
+      let str = new Cypher().match('n').withRelationship('r', { for: '12 years' }).build();
+      str.should.equal("MATCH (n)-[r { for: '12 years' }]-()");
+    });
+
+    it('should accept a unnamed relationship with only properties', function() {
+      let str = new Cypher().match('n').withRelationship({ for: '12 years' }).build();
+      str.should.equal("MATCH (n)-[{ for: '12 years' }]-()");
+    });
+  });
+
+  describe('.to', function() {
+    it('should make the previously relationship an outgoing one', function() {
+      let str = new Cypher().match('n').withRelationship().to('a').build();
+      str.should.equal('MATCH (n)-->(a)');
+    });
+
+    it('should work with properties', function() {
+      let str = new Cypher().match('n').withRelationship().to('a', { name: 'John' }).build();
+      str.should.equal("MATCH (n)-->(a { name: 'John' })");
+    });
+
+    it('should work with properties where the node is unnamed', function() {
+      let str = new Cypher().match('n').withRelationship().to({ name: 'John' }).build();
+      str.should.equal("MATCH (n)-->({ name: 'John' })");
+    });
+  });
+
+  describe('.from', function() {
+    it('should make the previously relationship an outgoing one', function() {
+      let str = new Cypher().match('n').withRelationship().from('a').build();
+      str.should.equal('MATCH (n)<--(a)');
+    });
+
+    it('should work with properties', function() {
+      let str = new Cypher().match('n').withRelationship().from('a', { name: 'John' }).build();
+      str.should.equal("MATCH (n)<--(a { name: 'John' })");
+    });
+
+    it('should work with properties where the node is unnamed', function() {
+      let str = new Cypher().match('n').withRelationship().from({ name: 'John' }).build();
+      str.should.equal("MATCH (n)<--({ name: 'John' })");
     });
   });
 });
